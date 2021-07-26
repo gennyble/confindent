1c74570adf9daf3bb75aa88b8d76ba5a601a0db4
Brad Alfirevic <brad@genbyte.dev>
2021-07-26 02:09:48 -0500

    Run cargo fmt

--- src/indent.rs
+++ src/indent.rs
@@ -1,118 +1,118 @@
 use std::{
-    ops::{Add, AddAssign},
-    str::FromStr,
+	ops::{Add, AddAssign},
+	str::FromStr,
 };
 
 use crate::error::{ParseError, ParseErrorKind};
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Indent {
-    Empty,
-    Tabs(usize),
-    Spaces(usize),
+	Empty,
+	Tabs(usize),
+	Spaces(usize),
 }
 
 impl Indent {
-    pub fn size(&self) -> usize {
-        match self {
-            Self::Empty => 0,
-            Self::Tabs(t) => *t,
-            Self::Spaces(t) => *t,
-        }
-    }
+	pub fn size(&self) -> usize {
+		match self {
+			Self::Empty => 0,
+			Self::Tabs(t) => *t,
+			Self::Spaces(t) => *t,
+		}
+	}
 }
 
 impl Add<usize> for Indent {
-    type Output = Indent;
+	type Output = Indent;
 
-    fn add(self, rhs: usize) -> Self::Output {
-        match self {
-            Indent::Tabs(cnt) => Indent::Tabs(cnt + rhs),
-            Indent::Spaces(cnt) => Indent::Spaces(cnt + rhs),
-            Indent::Empty => Indent::Empty,
-        }
-    }
+	fn add(self, rhs: usize) -> Self::Output {
+		match self {
+			Indent::Tabs(cnt) => Indent::Tabs(cnt + rhs),
+			Indent::Spaces(cnt) => Indent::Spaces(cnt + rhs),
+			Indent::Empty => Indent::Empty,
+		}
+	}
 }
 
 impl AddAssign<usize> for Indent {
-    fn add_assign(&mut self, rhs: usize) {
-        match self {
-            Indent::Tabs(cnt) => *cnt += rhs,
-            Indent::Spaces(cnt) => *cnt += rhs,
-            Indent::Empty => (),
-        }
-    }
+	fn add_assign(&mut self, rhs: usize) {
+		match self {
+			Indent::Tabs(cnt) => *cnt += rhs,
+			Indent::Spaces(cnt) => *cnt += rhs,
+			Indent::Empty => (),
+		}
+	}
 }
 
 impl FromStr for Indent {
-    type Err = ParseErrorKind;
+	type Err = ParseErrorKind;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.is_empty() {
-            return Ok(Indent::Empty);
-        } else if !s.trim().is_empty() {
-            // if we're not only dealing with whitespace, this is a programming error
-            panic!("Tried to pass Indent::from_str a string with something other than tabs/spaces");
-        }
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		if s.is_empty() {
+			return Ok(Indent::Empty);
+		} else if !s.trim().is_empty() {
+			// if we're not only dealing with whitespace, this is a programming error
+			panic!("Tried to pass Indent::from_str a string with something other than tabs/spaces");
+		}
 
-        let mut chars = s.chars();
-        let mut indent = match chars.next() {
-            Some(' ') => Indent::Spaces(1),
-            Some('\t') => Indent::Tabs(1),
-            _ => unreachable!(),
-        };
+		let mut chars = s.chars();
+		let mut indent = match chars.next() {
+			Some(' ') => Indent::Spaces(1),
+			Some('\t') => Indent::Tabs(1),
+			_ => unreachable!(),
+		};
 
-        for ch in chars {
-            match ch {
-                ' ' => match indent {
-                    Indent::Spaces(_) => indent += 1,
-                    Indent::Tabs(_) => return Err(ParseErrorKind::MixedIndent),
-                    _ => unreachable!(),
-                },
-                '\t' => match indent {
-                    Indent::Tabs(_) => indent += 1,
-                    Indent::Spaces(_) => return Err(ParseErrorKind::MixedIndent),
-                    _ => unreachable!(),
-                },
-                _ => unreachable!(),
-            }
-        }
+		for ch in chars {
+			match ch {
+				' ' => match indent {
+					Indent::Spaces(_) => indent += 1,
+					Indent::Tabs(_) => return Err(ParseErrorKind::MixedIndent),
+					_ => unreachable!(),
+				},
+				'\t' => match indent {
+					Indent::Tabs(_) => indent += 1,
+					Indent::Spaces(_) => return Err(ParseErrorKind::MixedIndent),
+					_ => unreachable!(),
+				},
+				_ => unreachable!(),
+			}
+		}
 
-        Ok(indent)
-    }
+		Ok(indent)
+	}
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn from_str() {
-        let empty = "";
-        let onetab = "\t";
-        let onespace = " ";
+	#[test]
+	fn from_str() {
+		let empty = "";
+		let onetab = "\t";
+		let onespace = " ";
 
-        assert_eq!(Indent::from_str(empty).unwrap(), Indent::Empty);
-        assert_eq!(Indent::from_str(onetab).unwrap(), Indent::Tabs(1));
-        assert_eq!(Indent::from_str(onespace).unwrap(), Indent::Spaces(1));
+		assert_eq!(Indent::from_str(empty).unwrap(), Indent::Empty);
+		assert_eq!(Indent::from_str(onetab).unwrap(), Indent::Tabs(1));
+		assert_eq!(Indent::from_str(onespace).unwrap(), Indent::Spaces(1));
 
-        let twotab = "\t\t";
-        let twospace = "  ";
+		let twotab = "\t\t";
+		let twospace = "  ";
 
-        assert_eq!(Indent::from_str(twotab).unwrap(), Indent::Tabs(2));
-        assert_eq!(Indent::from_str(twospace).unwrap(), Indent::Spaces(2));
+		assert_eq!(Indent::from_str(twotab).unwrap(), Indent::Tabs(2));
+		assert_eq!(Indent::from_str(twospace).unwrap(), Indent::Spaces(2));
 
-        let mixedwhitespace = "\t ";
+		let mixedwhitespace = "\t ";
 
-        assert_eq!(
-            Indent::from_str(mixedwhitespace).unwrap_err(),
-            ParseErrorKind::MixedIndent
-        )
-    }
+		assert_eq!(
+			Indent::from_str(mixedwhitespace).unwrap_err(),
+			ParseErrorKind::MixedIndent
+		)
+	}
 
-    #[test]
-    #[should_panic]
-    fn not_whitespace_panic() {
-        Indent::from_str(" a").unwrap();
-    }
+	#[test]
+	#[should_panic]
+	fn not_whitespace_panic() {
+		Indent::from_str(" a").unwrap();
+	}
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,207 +10,207 @@ pub use value::Value;
 
 #[derive(Debug, PartialEq)]
 pub struct Confindent {
-    children: Vec<Value>,
+	children: Vec<Value>,
 }
 
 impl Confindent {
-    pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, ParseError> {
-        let string = fs::read_to_string(path).map_err(|_| ParseError {
-            line: 0,
-            kind: ParseErrorKind::FileReadError,
-        })?;
+	pub fn from_file<P: AsRef<Path>>(path: P) -> Result<Self, ParseError> {
+		let string = fs::read_to_string(path).map_err(|_| ParseError {
+			line: 0,
+			kind: ParseErrorKind::FileReadError,
+		})?;
 
-        Confindent::from_str(&string)
-    }
+		Confindent::from_str(&string)
+	}
 
-    pub fn child<S: AsRef<str>>(&self, key: S) -> Option<&Value> {
-        for child in &self.children {
-            if child.key == key.as_ref() {
-                return Some(child);
-            }
-        }
+	pub fn child<S: AsRef<str>>(&self, key: S) -> Option<&Value> {
+		for child in &self.children {
+			if child.key == key.as_ref() {
+				return Some(child);
+			}
+		}
 
-        None
-    }
+		None
+	}
 
-    pub fn children<S: AsRef<str>>(&self, key: S) -> Vec<&Value> {
-        self.children
-            .iter()
-            .filter(|value| value.key == key.as_ref())
-            .collect()
-    }
+	pub fn children<S: AsRef<str>>(&self, key: S) -> Vec<&Value> {
+		self.children
+			.iter()
+			.filter(|value| value.key == key.as_ref())
+			.collect()
+	}
 
-    pub fn child_value<S: AsRef<str>>(&self, key: S) -> Option<&str> {
-        self.child(key).map(|child| child.value()).flatten()
-    }
+	pub fn child_value<S: AsRef<str>>(&self, key: S) -> Option<&str> {
+		self.child(key).map(|child| child.value()).flatten()
+	}
 
-    fn push(&mut self, value: Value) -> Result<(), ParseErrorKind> {
-        // Handle the easy stuff first
-        if Indent::Empty == value.indent {
-            self.children.push(value);
-            return Ok(());
-        } else if self.children.is_empty() {
-            return Err(ParseErrorKind::StartedIndented);
-        }
+	fn push(&mut self, value: Value) -> Result<(), ParseErrorKind> {
+		// Handle the easy stuff first
+		if Indent::Empty == value.indent {
+			self.children.push(value);
+			return Ok(());
+		} else if self.children.is_empty() {
+			return Err(ParseErrorKind::StartedIndented);
+		}
 
-        let mut curr = self.children.last_mut().unwrap();
-        match value.indent {
-            Indent::Tabs(tabsize) => loop {
-                match curr.children.last() {
-                    None => {
-                        curr.children.push(value);
-                        break;
-                    }
-                    Some(child) => match child.indent {
-                        Indent::Empty => unreachable!(),
-                        Indent::Spaces(_) => return Err(ParseErrorKind::TabsWithSpaces),
-                        Indent::Tabs(level) => {
-                            if tabsize == level {
-                                curr.children.push(value);
-                                break;
-                            } else {
-                                curr = curr.children.last_mut().unwrap();
-                            }
-                        }
-                    },
-                }
-            },
-            Indent::Spaces(size) => loop {
-                match curr.children.last() {
-                    None => {
-                        curr.children.push(value);
-                        break;
-                    }
-                    Some(child) => match child.indent {
-                        Indent::Empty => unreachable!(),
-                        Indent::Tabs(_) => return Err(ParseErrorKind::SpacesWithTabs),
-                        Indent::Spaces(level) => {
-                            if size == level {
-                                curr.children.push(value);
-                                break;
-                            } else {
-                                curr = curr.children.last_mut().unwrap();
-                            }
-                        }
-                    },
-                }
-            },
-            _ => unreachable!(),
-        }
+		let mut curr = self.children.last_mut().unwrap();
+		match value.indent {
+			Indent::Tabs(tabsize) => loop {
+				match curr.children.last() {
+					None => {
+						curr.children.push(value);
+						break;
+					}
+					Some(child) => match child.indent {
+						Indent::Empty => unreachable!(),
+						Indent::Spaces(_) => return Err(ParseErrorKind::TabsWithSpaces),
+						Indent::Tabs(level) => {
+							if tabsize == level {
+								curr.children.push(value);
+								break;
+							} else {
+								curr = curr.children.last_mut().unwrap();
+							}
+						}
+					},
+				}
+			},
+			Indent::Spaces(size) => loop {
+				match curr.children.last() {
+					None => {
+						curr.children.push(value);
+						break;
+					}
+					Some(child) => match child.indent {
+						Indent::Empty => unreachable!(),
+						Indent::Tabs(_) => return Err(ParseErrorKind::SpacesWithTabs),
+						Indent::Spaces(level) => {
+							if size == level {
+								curr.children.push(value);
+								break;
+							} else {
+								curr = curr.children.last_mut().unwrap();
+							}
+						}
+					},
+				}
+			},
+			_ => unreachable!(),
+		}
 
-        Ok(())
-    }
+		Ok(())
+	}
 }
 
 impl FromStr for Confindent {
-    type Err = ParseError;
+	type Err = ParseError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut ret = Self { children: vec![] };
-        let lines = s.lines().enumerate();
-        let add_ln =
-            |e: ParseErrorKind, ln: usize| -> ParseError { ParseError { line: ln, kind: e } };
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let mut ret = Self { children: vec![] };
+		let lines = s.lines().enumerate();
+		let add_ln =
+			|e: ParseErrorKind, ln: usize| -> ParseError { ParseError { line: ln, kind: e } };
 
-        for (line_number, line) in lines {
-            let value_op = Value::from_str(line).map_err(|e| add_ln(e, line_number))?;
+		for (line_number, line) in lines {
+			let value_op = Value::from_str(line).map_err(|e| add_ln(e, line_number))?;
 
-            match value_op {
-                Some(v) => ret.push(v).map_err(|e| add_ln(e, line_number))?,
-                None => continue,
-            };
-        }
+			match value_op {
+				Some(v) => ret.push(v).map_err(|e| add_ln(e, line_number))?,
+				None => continue,
+			};
+		}
 
-        Ok(ret)
-    }
+		Ok(ret)
+	}
 }
 
 #[cfg(test)]
 mod test {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn parses_single() {
-        let single = "Key Value";
+	#[test]
+	fn parses_single() {
+		let single = "Key Value";
 
-        assert_eq!(
-            Confindent::from_str(single).unwrap(),
-            Confindent {
-                children: vec![Value::new(Indent::Empty, "Key", Some("Value".into()))]
-            }
-        );
-    }
+		assert_eq!(
+			Confindent::from_str(single).unwrap(),
+			Confindent {
+				children: vec![Value::new(Indent::Empty, "Key", Some("Value".into()))]
+			}
+		);
+	}
 
-    #[test]
-    fn parses_double_noindent() {
-        let double = "Key1 Value1\nKey2 Value2";
+	#[test]
+	fn parses_double_noindent() {
+		let double = "Key1 Value1\nKey2 Value2";
 
-        assert_eq!(
-            Confindent::from_str(double).unwrap(),
-            Confindent {
-                children: vec![
-                    Value::new(Indent::Empty, "Key1", Some("Value1".into())),
-                    Value::new(Indent::Empty, "Key2", Some("Value2".into()))
-                ]
-            }
-        );
-    }
+		assert_eq!(
+			Confindent::from_str(double).unwrap(),
+			Confindent {
+				children: vec![
+					Value::new(Indent::Empty, "Key1", Some("Value1".into())),
+					Value::new(Indent::Empty, "Key2", Some("Value2".into()))
+				]
+			}
+		);
+	}
 
-    #[test]
-    fn parses_double_indented() {
-        let doubledent = "Key1 Value1\n\tKey2 Value2";
+	#[test]
+	fn parses_double_indented() {
+		let doubledent = "Key1 Value1\n\tKey2 Value2";
 
-        assert_eq!(
-            Confindent::from_str(doubledent).unwrap(),
-            Confindent {
-                children: vec![Value {
-                    indent: Indent::Empty,
-                    key: "Key1".into(),
-                    value: Some("Value1".into()),
-                    children: vec![Value::new(Indent::Tabs(1), "Key2", Some("Value2".into()))]
-                }]
-            }
-        );
-    }
+		assert_eq!(
+			Confindent::from_str(doubledent).unwrap(),
+			Confindent {
+				children: vec![Value {
+					indent: Indent::Empty,
+					key: "Key1".into(),
+					value: Some("Value1".into()),
+					children: vec![Value::new(Indent::Tabs(1), "Key2", Some("Value2".into()))]
+				}]
+			}
+		);
+	}
 
-    #[test]
-    fn parses_three_allindented() {
-        let doubledent = "Key1 Value1\n\tKey2 Value2\n\t\tKey3 Value3";
+	#[test]
+	fn parses_three_allindented() {
+		let doubledent = "Key1 Value1\n\tKey2 Value2\n\t\tKey3 Value3";
 
-        assert_eq!(
-            Confindent::from_str(doubledent).unwrap(),
-            Confindent {
-                children: vec![Value {
-                    indent: Indent::Empty,
-                    key: "Key1".into(),
-                    value: Some("Value1".into()),
-                    children: vec![Value {
-                        indent: Indent::Tabs(1),
-                        key: "Key2".into(),
-                        value: Some("Value2".into()),
-                        children: vec![Value::new(Indent::Tabs(2), "Key3", Some("Value3".into()))]
-                    }]
-                }]
-            }
-        );
-    }
+		assert_eq!(
+			Confindent::from_str(doubledent).unwrap(),
+			Confindent {
+				children: vec![Value {
+					indent: Indent::Empty,
+					key: "Key1".into(),
+					value: Some("Value1".into()),
+					children: vec![Value {
+						indent: Indent::Tabs(1),
+						key: "Key2".into(),
+						value: Some("Value2".into()),
+						children: vec![Value::new(Indent::Tabs(2), "Key3", Some("Value3".into()))]
+					}]
+				}]
+			}
+		);
+	}
 
-    #[test]
-    fn parses_three() {
-        let doubledent = "Key1 Value1\n\tKey2 Value2\nKey3 Value3";
+	#[test]
+	fn parses_three() {
+		let doubledent = "Key1 Value1\n\tKey2 Value2\nKey3 Value3";
 
-        assert_eq!(
-            Confindent::from_str(doubledent).unwrap(),
-            Confindent {
-                children: vec![
-                    Value {
-                        indent: Indent::Empty,
-                        key: "Key1".into(),
-                        value: Some("Value1".into()),
-                        children: vec![Value::new(Indent::Tabs(1), "Key2", Some("Value2".into()))]
-                    },
-                    Value::new(Indent::Empty, "Key3", Some("Value3".into()))
-                ]
-            }
-        );
-    }
+		assert_eq!(
+			Confindent::from_str(doubledent).unwrap(),
+			Confindent {
+				children: vec![
+					Value {
+						indent: Indent::Empty,
+						key: "Key1".into(),
+						value: Some("Value1".into()),
+						children: vec![Value::new(Indent::Tabs(1), "Key2", Some("Value2".into()))]
+					},
+					Value::new(Indent::Empty, "Key3", Some("Value3".into()))
+				]
+			}
+		);
+	}
 }
