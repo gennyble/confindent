cbb0aa4bc55c346f33e41f979e72cd3a95518a75
gennyble <gen@nyble.dev>
2024-09-09 16:41:55 -0500

    Macro uses inner

--- examples/read.rs
+++ examples/read.rs
@@ -1,16 +1,22 @@
-use confindent::{Confindent, Node};
+use confindent::{get, let_get, Confindent, Node};
 
 fn main() {
 	let conf = Confindent::from_file("examples/songinfo.conf").unwrap();
 	let song = conf.child("Song").unwrap();
-	let length: usize = song.child_parse("Length").unwrap();
+	//let length: usize = song.child_parse("Length").unwrap();
+	let_get!(
+		song,
+		clone artist = "Artist",
+		length: usize = "Length",
+		bitrate: usize = "Bitrate",
+	);
 
-	println!(
+	/*println!(
 		"Now playing {} by {} [{}:{} {}kbps]",
 		song.value().unwrap(),
-		song.child_value("Artist").unwrap(),
+		artist,
 		length / 60, //minutes
 		length % 60, //seconds
-		song.child_value("Bitrate").unwrap()
-	);
+		bitrate
+	);*/
 }
--- src/macros.rs
+++ src/macros.rs
@@ -10,18 +10,78 @@ macro_rules! get {
 	};
 }
 
-#[macro_export]
+//TODO: gen- Consider using $tt to preprocess tokens:
+// https://palant.info/2023/04/17/processing-a-complex-syntax-with-rusts-declarative-macros/
+/*#[macro_export]
 macro_rules! let_get {
 	($conf:ident / $varname:ident = $key:literal) => {
 		let $varname = $conf.get($key).unwrap().to_owned();
+	};
+
+	($conf:ident / catch { $catch:tt }; $varname:ident = $key:literal) => {
+		let Some($varname) = $conf.get($key).map(|v| v.to_owned()) else {
+			$catch
+		};
 	};
 
 	($conf:ident / $varname:ident: $vartype:ty = $key:literal) => {
 		let $varname: $vartype = $conf.child_parse($key).unwrap();
 	};
 
-	($conf:ident / $varname:ident$(: $vartype:ty)? = $key:literal, $($varnames:ident$(: $vartypes:ty)? = $keys:literal),+) => {
+	($conf:ident / catch { $catch:tt }; $varname:ident: $vartype:ty = $key:literal) => {
+		let Some($varname): $vartype = $conf.child_parse($key).ok().unwrap() else {
+			$catch
+		};
+	};
+
+	($conf:ident / $varname:ident$(: $vartype:ty)? = $key:literal, $($varnames:ident$(: $vartypes:ty)? = $keys:literal),+ $(,)?) => {
 		   let_get!($conf / $varname$(: $vartype)? = $key);
 		   let_get!($conf / $($varnames$(: $vartypes)? = $keys),+)
 	   };
+
+	($conf:ident / catch { $catch:tt }; $varname:ident$(: $vartype:ty)? = $key:literal, $($varnames:ident$(: $vartypes:ty)? = $keys:literal),+ $(,)?) => {
+		let_get!($conf / catch { $catch }; $varname$(: $vartype)? = $key);
+		let_get!($conf / catch { $catch }; $($varnames$(: $vartypes)? = $keys)+ $catch)
+	};
+}*/
+
+#[macro_export]
+macro_rules! let_get {
+	($conf:ident, $($tokens:tt)*) => {
+		confindent::let_get_inner!([] $conf, $($tokens)*)
+	};
+}
+
+#[macro_export]
+macro_rules! let_get_inner {
+	// A simple let... the borrows
+	// let key = conf.get("Value").unwrap();
+	{[$($processed:tt)*] $conf:ident, $varname:ident = $key:literal, $($rest:tt)*} => {
+		confindent::let_get_inner!{[
+			$($processed)*
+			let $varname = $conf.get($key).unwrap();
+		] $conf, $($rest)*}
+	};
+
+	// A simple let... that clones
+	// let key = conf.get("Value").unwrap().to_owned();
+	{[$($processed:tt)*] $conf:ident, clone $varname:ident = $key:literal, $($rest:tt)*} => {
+		confindent::let_get_inner!{[
+			$($processed)*
+			let $varname = $conf.child_owned($key).unwrap();
+		] $conf, $($rest)*}
+	};
+
+	// A let... but parsed to some type
+	// let key: usize = conf.child_parse("Value").unwrap();
+	{[$($processed:tt)*] $conf:ident, $varname:ident: $vartype:ty = $key:literal, $($rest:tt)*} => {
+		confindent::let_get_inner!{[
+			$($processed)*
+			let $varname: $vartype = $conf.child_parse($key).unwrap();
+		] $conf, $($rest)*}
+	};
+
+	{[$($processed:tt)*] $conf:ident,} => {
+		$($processed)*
+	}
 }
